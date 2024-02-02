f1028dc698fec535a1dc13418690a3bac061f5f4
miro-afk <128838750+miro-afk@users.noreply.github.com>
2024-02-02 20:20:35 +0300

    Починил подсчет создаваемых предметов

--- Assets/Scripts/GameControll/CreateResource.cs
+++ Assets/Scripts/GameControll/CreateResource.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using UnityEditor;
 using UnityEngine;
 
 
@@ -9,10 +10,14 @@ public class CreateResource : MonoBehaviour
     public GameObject prefab;
     public Canvas popup;
     public TMP_Text textMeshPro;
-    bool IsFirstTime = true; //Это надо где-то хранить, может создать статическую переменную к ресурсам? либо наследовать класс для каждого ресурса?
+    Resource resource;
     double time = 0;
-    
 
+
+    public void Start()
+    {
+        resource = prefab.GetComponent<Resource>();
+    }
     // Update is called once per frame
     public void Update()
     {
@@ -20,7 +25,7 @@ public class CreateResource : MonoBehaviour
         //по нажатию мышки
         if (Input.GetMouseButtonDown(0))
         {
-            if (IsFirstTime)
+            if (resource.count==0)
             {
                 CreatePopup();
             }
@@ -44,14 +49,13 @@ public class CreateResource : MonoBehaviour
         textMeshPro.text = "Было создано дерево"; //текст к каждому действию должен быть разный, переменную в ресурсе, куча if либо наследовать класс для каждого ресурса? 
         popup.gameObject.SetActive(true);
         //Поле исчезает через 10 секунд, сделать по нажатию?
-        time = 10;
-        IsFirstTime = false;
-        
+        time = 10;       
     }
 
     //создаёт ресурс
     private void SpawnPrefab()
     {
+        resource.count += 1;
         Vector2 target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         Instantiate(prefab, target, Quaternion.identity);
     }
