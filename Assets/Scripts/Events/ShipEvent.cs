7242fa4b17f60ee26c6b8f11930beef0208f50d2
miro-afk <128838750+miro-afk@users.noreply.github.com>
2024-02-06 14:54:35 +0300

    Добавил сцену экрана победы

--- Assets/Scripts/Events/ShipEvent.cs
+++ Assets/Scripts/Events/ShipEvent.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace Events
 {
@@ -7,6 +8,8 @@ namespace Events
         private void Start()
         {
             Survivor.Instance.ChangeParameter(5, 30);
+            //Запуск победной сцены, если сделать не моментально после создания, то надо переместить
+            SceneManager.LoadScene("VictoryScreen");
         }
     }
 }
